27af97f0e26d745abd14fda027c36de268bca0d3
RoshanCS <roshan.cs790@gmail.com>
2020-12-25 16:51:21 +0530

    rh function added
    humidity read function added

--- aht10_trh.ino
+++ aht10_trh.ino
@@ -20,34 +20,36 @@ void loop()
   Wire.beginTransmission(I2C_ADDR);
   Wire.write(TRIG_MEASURE);
   Wire.endTransmission();
-  delay(200);
+  delay(70);
 
   Wire.requestFrom(I2C_ADDR, 6);
- 
+  
   for(int i = 0; i < 6; i++)
   {
   	readByte[i] = Wire.read();
-  	//Serial.print(readByte[i], BIN);
-  	//Serial.print(" | ");
   }
 
-  //sprintgbrs();
-
   Serial.print(tempRead());
+  Serial.print(" | ");
+  Serial.print(rhRead());
+
   Serial.println();
   delay(3000);
 
 }
 
-void sprintgbrs()
+double rhRead()
 {
-	uint8_t gbrsByte = 2; 
-	uint8_t gbrsByte2 = 3;
-	uint32_t bigVal = (uint32_t) gbrsByte << 16 | gbrsByte2 << 8 | 7;
-	Serial.print(bigVal, BIN);
-	//Serial.print(" | ");
-	//Serial.print((uint32_t) gbrsByte << 16 | 2, BIN);
-	Serial.println();
+	uint32_t rhData;
+	
+	for(int8_t i = 0; i < 2; i++)
+	{
+		rhData |= readByte[1 + i];
+		rhData = rhData << 4;
+	}
+	rhData = (rhData << 4) | (readByte[3] >> 4);
+	double rh = (double) rhData * 0.000095;
+	return rh;
 }
 
 double tempRead()
@@ -59,6 +61,6 @@ double tempRead()
 	}
 	//uint32_t tempData = ((uint32_t) readByte[3] & 0x0F) << 16 | ((uint16_t) readByte[4]) << 8 | readByte[5];
 	
-	double temp = (double) tempData * 0.00019 - 50;
+	double temp = (double) tempData * 0.000191 - 50;
 	return temp;
 }
